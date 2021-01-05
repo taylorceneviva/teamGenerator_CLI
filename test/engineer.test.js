0d7992d25c1523827615a93c9a8a164aab9fbc5a
Taylor Ceneviva <taylor.ceneviva@gmail.com>
2021-01-05 16:38:34 -0500

    added test template

--- test/engineer.test.js
+++ test/engineer.test.js
@@ -1,19 +1,19 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can set GitHUb account via constructor", () => {
-  const testValue = "GitHubUser";
+  const testValue = "github";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.github).toBe(testValue);
 });
 
 test("getRole() should return \"Engineer\"", () => {
   const testValue = "Engineer";
-  const e = new Engineer("Foo", 1, "test@test.com", "GitHubUser");
+  const e = new Engineer("Foo", 1, "test@test.com", "github");
   expect(e.getRole()).toBe(testValue);
 });
 
 test("Can get GitHub username via getGithub()", () => {
-  const testValue = "GitHubUser";
+  const testValue = "github";
   const e = new Engineer("Foo", 1, "test@test.com", testValue);
   expect(e.getGithub()).toBe(testValue);
 });
